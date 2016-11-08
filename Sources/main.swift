5238cdb6680c22ac046ee202fea5eb7aa68888e7
king <king@kings-MacBook-Pro.local>
2016-11-08 13:27:43 -0600

    update main with timestamp backend

--- Sources/main.swift
+++ Sources/main.swift
@@ -13,6 +13,43 @@ let router = Router()
 router.all("/", middleware: StaticFileServer(path: "./public"))
 
 //
+//	Timestamps
+//
+//	Add REST endpoints for getting, setting and clearing timestamps in an array in memory
+//
+var timestamps = [String]()
+
+router.get("/timestamps") {
+    request, response, next in
+    response.send(json: JSON(timestamps))
+    next()
+}
+
+router.post("/timestamps") {
+    request, response, next in
+    
+    let formatter = DateFormatter()
+    formatter.dateStyle = .medium
+    formatter.timeStyle = .long
+    
+    var dateStr = formatter.string(from: Date())
+    timestamps.append(dateStr)
+    
+    response.status(.OK)
+    next()
+}
+
+router.delete("/timestamps") {
+    request, response, next in
+    
+    timestamps = []
+    
+    response.status(.OK)
+    next()
+}
+
+
+//
 // Read environment variables and look for port we should listen on
 //
 let envVars = ProcessInfo.processInfo.environment
